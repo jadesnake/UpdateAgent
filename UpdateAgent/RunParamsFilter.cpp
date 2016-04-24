d12a8c0da197f2ba45a8b19f76706281e50daab0
贾延辉 <jadesoft@hotmail.com>
2016-04-24 20:09:09 +0800

    提交更改

--- UpdateAgent/RunParamsFilter.cpp
+++ UpdateAgent/RunParamsFilter.cpp
@@ -5,8 +5,28 @@
 RunParamsFilter::RunParamsFilter()
 {
 }
-
-
 RunParamsFilter::~RunParamsFilter()
 {
+}
+void RunParamsFilter::addFilter(const FilterFun& filter) 
+{
+	mFilters_.push_back(filter);
+}
+void RunParamsFilter::runParser(LPCTSTR lpCmdLine)
+{
+	if (lpCmdLine == nullptr)
+		return;
+	CString strCmd,strValue;
+	bool    bCmp = false;
+	size_t szTotal = _tclen(lpCmdLine);
+	for (size_t nI = 0; nI < szTotal;nI++) {
+		TCHAR chA = lpCmdLine[szTotal];
+		if (chA != ':' && bCmp==false) {
+			strCmd += chA;
+		}
+		else {
+			bCmp = true;
+		}
+	}
+
 }
